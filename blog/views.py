563553ad6cc05c6841e739e43bdca8dde5cff3ba
nataliatorq <torquato.natalia@escolar.ifrn.edu.br>
2024-08-30 07:46:53 -0300

    views

--- blog/views.py
+++ blog/views.py
@@ -5,14 +5,15 @@ from .models import GostosPessoais, Curiosidades, SobreMim
 def home(request):
     gostos_pessoais = GostosPessoais.objects.all()
     curiosidades = Curiosidades.objects.all()
-    return render(request, 'blog/home.html', {
+    context = {
         'gostos_pessoais': gostos_pessoais,
         'curiosidades': curiosidades
-    })
+    }
+    return render(request, 'blog/home.html', context)
 
 def sobre(request):
     sobre_mim = SobreMim.objects.first()  # Pegando a primeira instância
-    return render(request, 'blog/sobre.html', {
+    context = {
         'sobre_mim': sobre_mim
-    })
-
+    }
+    return render(request, 'blog/sobre.html', context)
